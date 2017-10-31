58dab92b6d6aa267f00fea1f72f4ea062669c14f
wint3rmute <mateusz.baczek1998@gmail.com>
2017-10-31 15:40:39 +0100

    not everything, lol

--- app/src/main/java/com/example/krzysiek/choobovniak/MainActivity.java
+++ app/src/main/java/com/example/krzysiek/choobovniak/MainActivity.java
@@ -1,18 +1,37 @@
 package com.example.krzysiek.choobovniak;
 
+import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import me.angrybyte.goose.Article;
+import me.angrybyte.goose.ContentExtractor;
+import me.angrybyte.goose.network.GooseDownloader;
+
+import static android.content.ContentValues.TAG;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    Configuration conf;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -22,8 +41,20 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                try {
+                    URL a = new URL("https://www.wired.com/story/iphone-x-camera-and-faceid-test/");
+
+                    ExtractArticle extractArticle = new ExtractArticle();
+
+                    extractArticle.execute(a);
+
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+
+
             }
         });
     }
@@ -47,6 +78,46 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+
         return super.onOptionsItemSelected(item);
     }
+
+
+    class ExtractArticle extends AsyncTask<URL, Integer, Long> {
+        protected Long doInBackground(URL... urls) {
+            int count = urls.length;
+
+
+            me.angrybyte.goose.Configuration conf = new me.angrybyte.goose.Configuration(getApplicationContext().getCacheDir().toString());
+            ContentExtractor extractor = new ContentExtractor(conf);
+
+            Article article = extractor.extractContent(urls[0].toString(),true );
+            if (article == null) {
+                Log.e(TAG, "Couldn't load the article, is your URL correct, is your Internet working?");
+                return (long)1;
+            }
+
+
+            String details = article.getCleanedArticleText();
+            if (details == null) {
+                Log.w(TAG, "Couldn't load the article text, the page is messy. Trying with page description...");
+                details = article.getMetaDescription();
+            }
+            Log.e("Article", details);
+
+
+
+
+            return (long)0;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+
+        }
+
+        protected void onPostExecute(Long result) {
+
+        }
+    }
+
 }
